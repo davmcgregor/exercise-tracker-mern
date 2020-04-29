f2fca06e455fefd9cf98169cbe8c98a86ddf9c0f
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-29 19:07:39 +0100

    Stub other components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as ExercisesList } from './ExercisesList/ExercisesList';
+export { default as EditExercise } from './EditExercise/EditExercise';
+export { default as CreateExercise } from './CreateExercise/CreateExercise';
+export { default as CreateUser } from './CreateUser/CreateUser';
+
